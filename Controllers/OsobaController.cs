f953d3e3be38cc2c49e74ceefb60b848a1127850
ArturMarszalek97 <amarszalek97@wp.pl>
2019-02-26 16:37:59 +0100

    Add files via upload

---
+++ Controllers/OsobaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Configuration;
+using WebApplication1.Models;
+using WebApplication1.ViewModels.Osoby;
+
+namespace WebApplication1.Controllers
+{
+    public class OsobaController : Controller
+    {
+        // GET: Osoba
+        public ActionResult Index()
+        {
+            List<Osoba> lista = new List<Osoba>();
+            int licz = 1;
+
+            string sciezka = ConfigurationManager.AppSettings["CSV"].ToString();
+            StreamReader sr = new StreamReader(sciezka);
+
+            while(!sr.EndOfStream)
+            {
+                var wiersz = sr.ReadLine();
+                string[] podziel = wiersz.Split(',');
+
+                Osoba nowa = new Osoba(podziel[0], podziel[1], Convert.ToInt32(podziel[2]), licz);
+                licz++;
+                lista.Add(nowa);
+            }
+
+            OsobaVM osoba = new OsobaVM();
+            osoba.dane = lista;
+
+            
+            
+            return View(osoba);
+        }
+    }
+}
---
+++ Models/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int indeks;
+        public int wiersz;
+
+        public Osoba(string imie, string nazwisko, int indeks, int wiersz)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.indeks = indeks;
+            this.wiersz = wiersz;
+        }
+
+        public Osoba()
+        {
+
+        }
+    }
+}
